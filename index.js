8cdf02d1b14b38e01cdff44fae54eb39ab251cc2
prem singh <premrathore654@gmail.com>
2024-11-12 13:50:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+
+$(document).ready(function(){
+    $a = $("#h");
+    $b = $(".h1");
+    console.log($a, $b)
+
+   $c =  $("p").hide()
+   console.log($c)
+
+   $btn =  $("#btn");
+
+   $btn.click(function(){
+    // alert("button clicked")
+    $a.css({
+        "color": "red"
+    })
+   })
+
+
+//    $btn.dblclick(function(){
+//     alert("button clicked")
+//    })
+
+
+
+$btn.mouseenter(function(){
+    // alert("kjbjkbvj")
+    $b.hide()
+})
+$btn.dblclick(function(){
+    // alert("kjbjkbvj")
+    $b.show();
+
+})
+
+$inp =  $("#inp");
+$body =  $("#body");
+$inp.keypress(function(){
+$body.css({
+    "background": "red"
+})
+
+
+$inp.keyup(function(){
+    $body.css({
+        "background": "black"
+    })
+})
+});
+
+$inp.keydown(function(){
+    $body.css({
+        "background": "orange"
+    })
+})
+
+
+})
+
+
+
+
