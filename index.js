a6f6d04cee9a65b664f00dcaed3ca5ba74585440
prem singh <premrathore654@gmail.com>
2024-11-13 14:06:10 +0530

    c

--- index.js
+++ index.js
@@ -1,62 +1,110 @@
 
 
 
-$(document).ready(function(){
-    $a = $("#h");
-    $b = $(".h1");
-    console.log($a, $b)
+// $(document).ready(function(){
+//     $a = $("#h");
+//     $b = $(".h1");
+//     console.log($a, $b)
 
-   $c =  $("p").hide()
-   console.log($c)
+//    $c =  $("p").hide()
+//    console.log($c)
 
-   $btn =  $("#btn");
+//    $btn =  $("#btn");
 
-   $btn.click(function(){
-    // alert("button clicked")
-    $a.css({
-        "color": "red"
-    })
-   })
-
-
-//    $btn.dblclick(function(){
-//     alert("button clicked")
+//    $btn.click(function(){
+//     // alert("button clicked")
+//     $a.css({
+//         "color": "red"
+//     })
 //    })
 
 
-
-$btn.mouseenter(function(){
-    // alert("kjbjkbvj")
-    $b.hide(2000)
-})
-$btn.dblclick(function(){
-    // alert("kjbjkbvj")
-    $b.show(1000);
-
-})
-
-$inp =  $("#inp");
-$body =  $("#body");
-$inp.keypress(function(){
-$body.css({
-    "background": "red"
-})
-
-
-$inp.keyup(function(){
-    $body.css({
-        "background": "black"
-    })
-})
-});
-
-$inp.keydown(function(){
-    $body.css({
-        "background": "orange"
-    })
-})
-})
+// //    $btn.dblclick(function(){
+// //     alert("button clicked")
+// //    })
 
 
 
+// $btn.mouseenter(function(){
+//     // alert("kjbjkbvj")
+//     $b.hide(2000)
+// })
+// $btn.dblclick(function(){
+//     // alert("kjbjkbvj")
+//     $b.show(1000);
+
+// })
+
+// $inp =  $("#inp");
+// $body =  $("#body");
+// $inp.keypress(function(){
+// $body.css({
+//     "background": "red"
+// })
+
+
+// $inp.keyup(function(){
+//     $body.css({
+//         "background": "black"
+//     })
+// })
+// });
+
+// $inp.keydown(function(){
+//     $body.css({
+//         "background": "orange"
+//     })
+// })
+// })
+
+
+// $(document).ready(function(){
+//     // $btn =  $("#btn");
+//     // $p =  $("#data");
+//     // $btn.click(function(){
+//     //     // $p.hide(3000)
+//     //     $p.toggle("slow", function(){
+
+//     //     })
+//     // })
+
+//             $form =  $("#form");
+
+//             $p =  $("<p></p>").text("hello world")
+//             $h1 =  $("<h1>hello world</h1>");
+//             $button =  $("#button")
+//             // console.log($button.append($p))
+//             // console.log($button.append($h1))
+//             // console.log($button.prepend($p))
+//             // console.log($button.prepend($h1))
+
+
+
+
+//             $form.submit(function(e){
+//             e.preventDefault();
+//             $inp =  $("#inp").val()
+//             $pass =  $("#pass").val()
+//             $cpass =  $("#cpass").val()
+//             // text();
+//             //  html()
+          
+                
+                
+            
+
+//                     if($inp =="" || $pass =="" || $cpass==""){
+//                         alert("all fields are required")
+//                     }else{
+                       
+//                         if($pass !==$cpass){
+//                             alert('password mismatch')
+//                         }else{
+//                             alert("ok")
+//                         }
+//                     }               
+                
+//             })
+
+// })
 
